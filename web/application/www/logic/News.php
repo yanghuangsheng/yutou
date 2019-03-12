820e52d4bc92dae5e9a469c5d803271158108d8a
yanghuangsheng <260287826@qq.com>
2019-03-12 18:22:58 +0800

    201903121822

--- web/application/www/logic/News.php
+++ web/application/www/logic/News.php
@@ -51,7 +51,7 @@ class News extends Base
      */
     public function sevenDayTopData()
     {
-        return (new newsService)->hotData(30, 3, [['PortalNews.recommended','=',1]]);
+        return (new newsService)->hotData(30, 10, [['PortalNews.recommended','=',1]]);
     }
 
     /**
