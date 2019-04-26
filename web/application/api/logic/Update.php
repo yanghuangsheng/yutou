a3e69665a9dbbeb3c89f4b9f0eb30cf1ad0732da
yanghuangsheng <260287826@qq.com>
2019-04-26 17:31:17 +0800

    201904261731

--- web/application/api/logic/Update.php
+++ web/application/api/logic/Update.php
@@ -16,7 +16,7 @@ class Update extends Base
         $data = [
             'content' => '更新...',
             'wgt_url' => '',//$this->getDomain() . '/app_update/app-20190426.wgt',
-            'pkg_url' => $this->getDomain() . '/app_update/app-20190426.pak',
+            'pkg_url' => 'http://m.bh3721.com/app_update/app-20190426.pak',
         ];
         return showResult(0, '', $data);
     }
