b2f9f152f3069de056f0afd81296dc246fd3aa49
杨皇生 <yanghuangsheng@yanghuangshengdeMacBook-Air.local>
2019-06-05 14:55:41 +0800

    201906051455

--- web/application/api/logic/Match.php
+++ web/application/api/logic/Match.php
@@ -62,10 +62,10 @@ class Match extends Base
         $indField = ['support_o_num','support_main_num','support_passenger_num'];
         $incResult = $matchService->updateInc(['id', $saveData['match_id']], $indField[$saveData['support_status']]);
         //系统通知
-        $matchData = (new MatchService)->getFiles($saveData('match_id'), 'name, open_time', 1);
+        $matchData = (new MatchService)->getFiles($saveData('match_id'), 'name,open_time', 1);
         $msgResult = (new SystemMessageService)->toUserSystem(
             $saveData['user_id'],
-            ['content' => $matchData['name'] . '将于' . date('Y-m-d H:i:s', $matchData['open_time'] + 6000) . '准备开奖，敬请期待。'],
+            [$matchData['name'] . '将于' . date('Y-m-d H:i:s', $matchData['open_time'] + 6000) . '准备开奖，敬请期待。'],
             1
         );
 
