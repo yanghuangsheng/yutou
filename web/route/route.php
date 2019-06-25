6ba98eeb1493e50ecd81af17c4f614d0133e813d
杨皇生 <yanghuangsheng@yanghuangshengdeMacBook-Air.local>
2019-06-25 11:08:09 +0800

    201906251108

--- web/route/route.php
+++ web/route/route.php
@@ -114,7 +114,7 @@ Route::group('api', function () {
 
 
     Route::get(':ver/user/check_today', 'api/:ver.user/checkTodaySign'); //是否已签到
-    Route::post(':ver/user/today_sign', 'api/:ver.user/todaySign'); //签到
+    Route::post(':ver/user/sign_today', 'api/:ver.user/todaySign'); //签到
     Route::get(':ver/user/today_details', 'api/:ver.user/todayDetails'); //签到详情
 
 
