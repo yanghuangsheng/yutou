1b1ff3400538d63ba9d9bdc979157f456d7e5566
杨皇生 <yanghuangsheng@yanghuangshengdeMacBook-Air.local>
2019-06-25 14:30:08 +0800

    201906251430

--- web/application/api/service/PortalNewsComment.php
+++ web/application/api/service/PortalNewsComment.php
@@ -11,6 +11,7 @@ namespace app\api\service;
 
 class PortalNewsComment extends Common
 {
+    protected $resetReply = false;
     //初始化类
     public function __construct()
     {
@@ -25,6 +26,8 @@ class PortalNewsComment extends Common
      */
     protected function setWithOnView()
     {
+        $this->resetReply = true;
+
         return $this->model->view('PortalNewsComment', 'id,parent_id,user_id,news_id,content,create_time as date_time')
             ->view('User', ['name'=>'user_name', 'avatar'=>'user_avatar'], 'User.id = PortalNewsComment.user_id', 'LEFT')
             ->view('User reply', ['name'=>'reply_name', 'avatar'=>'reply_avatar'], 'reply.id = PortalNewsComment.reply_user_id', 'LEFT')
@@ -76,9 +79,10 @@ class PortalNewsComment extends Common
     protected function resetListData($data)
     {
         foreach ($data as $key => &$value){
+            if($this->resetReply == true){
+                $value['reply_avatar'] = $value['reply_avatar'] ? json_decode($value['reply_avatar'],1) :userAvatar();
+            }
             $value['user_avatar'] = $value['user_avatar'] ? json_decode($value['user_avatar'],1) :userAvatar();
-            $value['reply_avatar'] = $value['reply_avatar'] ? json_decode($value['reply_avatar'],1) :userAvatar();
-
             $value['date_time'] = friendlyDate($value['date_time']);
 
         }
