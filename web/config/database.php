b765885ec4dff9c25642aec0468f5cf9ad9c5256
yanghuangsheng <260287826@qq.com>
2019-10-15 08:15:32 +0800

    修改数据库地址

--- web/config/database.php
+++ web/config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'db_host',
+    'hostname'        => 'mysql',
     // 数据库名
     'database'        => 'yutou_db',
     // 用户名
